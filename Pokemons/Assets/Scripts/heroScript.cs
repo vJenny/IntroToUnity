916a0688be680db55fb1174ab2b1ffd5f0e4b7f1
vJenny <jana_valieva@list.ru>
2016-10-25 21:41:11 +0300

    Add hero's motion script

---
+++ Pokemons/Assets/Scripts/heroScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class heroScript : MonoBehaviour
+{
+    // переменные, доступные в Unity
+    public float MaxSpeed = 10f;
+    public LayerMask Ground;
+    public float HLocation;
+    public Transform GroundDetector;
+
+    // вспомогательные переменные
+
+    // константы
+
+    void Start () { }
+
+    void FixedUpdate()
+    {
+        HLocation = Input.GetAxis("Horizontal");
+    }
+
+    void Update()
+    {
+        var hero = GetComponent<Rigidbody2D>();
+        hero.velocity = new Vector2(HLocation * MaxSpeed, hero.velocity.y);
+    }
+}
