82b211dfba8c9d73182d75ff3b0fc0598496d5a2
vJenny <jana_valieva@list.ru>
2016-10-25 21:59:59 +0300

    Add jumps ability

--- Pokemons/Assets/Scripts/heroScript.cs
+++ Pokemons/Assets/Scripts/heroScript.cs
@@ -7,23 +7,32 @@ public class heroScript : MonoBehaviour
     // переменные, доступные в Unity
     public float MaxSpeed = 10f;
     public LayerMask Ground;
-    public float HLocation;
     public Transform GroundDetector;
 
+    public float JumpForce = 500f;
+
     // вспомогательные переменные
+    private float _hLocation;
+    private bool _grounded;
 
     // константы
+    private const float CheckRatio = 0.2f;
 
     void Start () { }
 
     void FixedUpdate()
     {
-        HLocation = Input.GetAxis("Horizontal");
+        _grounded = Physics2D.OverlapCircle(GroundDetector.position, CheckRatio, Ground); // определяем пересечение с землей
+        _hLocation = Input.GetAxis("Horizontal");
     }
 
     void Update()
     {
         var hero = GetComponent<Rigidbody2D>();
-        hero.velocity = new Vector2(HLocation * MaxSpeed, hero.velocity.y);
+
+        if (_grounded && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow)))
+            hero.AddForce(new Vector2(0f, JumpForce)); // добавляем движение вверх, 0 по оси Ох
+
+        hero.velocity = new Vector2(_hLocation * MaxSpeed, hero.velocity.y);
     }
 }
