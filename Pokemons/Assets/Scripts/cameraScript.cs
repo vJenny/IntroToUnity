609c2e50a426b60959b22e7c7e1e67636b416efc
vJenny <jana_valieva@list.ru>
2016-10-26 19:02:51 +0300

    Set up camera

---
+++ Pokemons/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{ 
+    public GameObject Hero;
+    private Vector3 _offset;         
+    
+    void Start()
+    {
+        // вычисляем отступ между позицией героя и позицией камеры
+        _offset = transform.position - Hero.transform.position;
+    }
+    
+    void LateUpdate()
+    {
+        // устанавливаем позицию камеры такую же, как у героя, учитывая исходный отступ
+        transform.position = Hero.transform.position + _offset;
+    }
+}
